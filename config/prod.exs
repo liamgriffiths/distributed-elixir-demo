202747a43f6da965380044808d2e708625c21866
Liam Griffiths <liamgriffiths@gmail.com>
2018-03-17 13:30:22 -0400

    Update prod config (use env, static content)

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :hello, HelloWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  http: [port: "${PORT}"],
+  check_origin: false,
+  server: true,
+  root: ".",
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
