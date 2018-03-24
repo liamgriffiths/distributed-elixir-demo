a74b51c816ecfc64cd1edc6bd25707e3a846fec9
Liam Griffiths <liamgriffiths@gmail.com>
2018-03-24 15:15:16 -0400

    Rm more wobserver stuff

--- lib/hello_web/endpoint.ex
+++ lib/hello_web/endpoint.ex
@@ -2,7 +2,6 @@ defmodule HelloWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :hello
 
   socket "/socket", HelloWeb.UserSocket
-  socket "/wobserver", Wobserver.Web.PhoenixSocket
 
   # Serve at "/" the static files from "priv/static" directory.
   #
